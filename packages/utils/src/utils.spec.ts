9f7893614869a8f2f00c3c43d1f536ee418450d9
Lance Miller <github@lance.pub>
2019-12-08 00:34:44 -0700

    test(utils): cleanup

--- packages/utils/src/utils.spec.ts
+++ packages/utils/src/utils.spec.ts
@@ -1,12 +1,5 @@
-class Something {
-	static test(arg: string): string {
-		return `Something.test() => ${ arg }`;
-	}
-}
-
 describe('Some', () => {
 	test('thing', () => {
-		console.log(Something.test('stuff'));
 		expect(true).toBe(true);
 	});
 });
