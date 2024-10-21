cd5d6a60d7c1898ca6229d8a120d4261bb5b7f9e
Lance Miller <lance@miller.pub>
2024-10-21 12:26:43 -0600

    configs

--- eslint.config.js
+++ eslint.config.js
@@ -30,4 +30,162 @@ module.exports = [
     // Override or add rules here
     rules: {},
   },
+  // {
+  //   plugins: [
+  //     "prettier",
+  //     "import",
+  //     "tailwindcss"
+  //   ],
+  //   extends: [
+  //     "plugin:prettier/recommended",
+  //     "plugin:import/recommended",
+  //     "plugin:import/typescript",
+  //     "plugin:tailwindcss/recommended"
+  //   ],
+  //   "rules": {
+  //     "@typescript-eslint/no-empty-interface": "off",
+  //     "@typescript-eslint/consistent-type-imports": [
+  //       "error",
+  //       {
+  //         "prefer": "type-imports",
+  //         "fixStyle": "inline-type-imports",
+  //         "disallowTypeAnnotations": false
+  //       }
+  //     ],
+  //     "@nx/enforce-module-boundaries": "off",
+  //     "import/exports-last": "off",
+  //     "import/first": "error",
+  //     "import/no-deprecated": "error",
+  //     "import/consistent-type-specifier-style": [
+  //       "error",
+  //       "prefer-inline"
+  //     ],
+  //     "import/newline-after-import": [
+  //       "error",
+  //       {
+  //         "considerComments": true
+  //       }
+  //     ],
+  //     "import/no-useless-path-segments": [
+  //       "error",
+  //       {
+  //         "noUselessIndex": true
+  //       }
+  //     ],
+  //     "import/order": [
+  //       "error",
+  //       {
+  //         "newlines-between": "always",
+  //         "distinctGroup": true,
+  //         "pathGroups": [
+  //           {
+  //             "pattern": "*.css",
+  //             "group": "index",
+  //             "position": "after"
+  //           },
+  //           {
+  //             "pattern": "@/**",
+  //             "group": "internal",
+  //             "position": "after"
+  //           }
+  //         ],
+  //         "groups": [
+  //           [
+  //             "builtin",
+  //             "external"
+  //           ],
+  //           "internal",
+  //           [
+  //             "parent",
+  //             "sibling",
+  //             "index"
+  //           ]
+  //         ],
+  //         "alphabetize": {
+  //           "order": "asc",
+  //           "caseInsensitive": true,
+  //           "orderImportKind": "asc"
+  //         }
+  //       }
+  //     ],
+  //   },
+  //   "overrides": [
+  //     {
+  //       "files": [
+  //         "*.ts",
+  //         "*.tsx",
+  //         "*.js",
+  //         "*.jsx"
+  //       ],
+  //       "rules": {
+  //         "@nx/enforce-module-boundaries": [
+  //           "error",
+  //           {
+  //             "enforceBuildableLibDependency": true,
+  //             "allow": [],
+  //             "depConstraints": [
+  //               {
+  //                 "sourceTag": "*",
+  //                 "onlyDependOnLibsWithTags": [
+  //                   "*"
+  //                 ]
+  //               }
+  //             ]
+  //           }
+  //         ]
+  //       }
+  //     },
+  //     {
+  //       "files": [
+  //         "*.ts",
+  //         "*.tsx"
+  //       ],
+  //       "extends": [
+  //         "plugin:@nx/typescript"
+  //       ],
+  //       "rules": {}
+  //     },
+  //     {
+  //       "files": [
+  //         "*.js",
+  //         "*.jsx"
+  //       ],
+  //       "extends": [
+  //         "plugin:@nx/javascript"
+  //       ],
+  //       "rules": {}
+  //     },
+  //     {
+  //       "files": [
+  //         "*.spec.ts",
+  //         "*.spec.tsx",
+  //         "*.spec.js",
+  //         "*.spec.jsx"
+  //       ],
+  //       "env": {
+  //         "jest": true
+  //       },
+  //       "rules": {}
+  //     }
+  //   ],
+  //   "settings": {
+  //     "import/extensions": [
+  //       ".js",
+  //       ".jsx",
+  //       ".ts",
+  //       ".tsx"
+  //     ],
+  //     "import/resolver": {
+  //       "typescript": true,
+  //       "node": true
+  //     },
+  //     "import/parsers": {
+  //       "@typescript-eslint/parser": [
+  //         ".ts",
+  //         ".tsx"
+  //       ]
+  //     },
+  //     "import/internal-regex": "^@astolat/"
+  //   }
+  // }
 ];
