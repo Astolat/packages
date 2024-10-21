b9304fecbbfebbf9ad25cf154a3183028b23007f
Lance Miller <lance@miller.pub>
2024-10-21 13:12:20 -0600

    mo betta formatting

--- apps/miller.pub-e2e/playwright.config.ts
+++ apps/miller.pub-e2e/playwright.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
 
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
+/** Read environment variables from file. https://github.com/motdotla/dotenv */
 // require('dotenv').config();
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+/** See https://playwright.dev/docs/test-configuration. */
 export default defineConfig({
 	...nxE2EPreset(__filename, { testDir: './src' }),
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
